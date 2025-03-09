13482e3ccc0a4aa42d4b3c0ebb7cae1d069846c6
marquenalensing290 <marquenalensing290@gmail.com>
2025-03-09 01:35:07 +0000

    Add 4.go

---
+++ src/4.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 10
+	num2 := 5
+	sum := num1 + num2
+	diff := num1 - num2
+	prod := num1 * num2
+	quotient := num1 / num2
+	remainder := num1 % num2
+	fmt.Println(sum, diff, prod, quotient, remainder)
+}
