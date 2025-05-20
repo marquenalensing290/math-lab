fc5e8fed1542109b9e7b1cefbd999c0a79dff508
marquenalensing290 <marquenalensing290@gmail.com>
2025-05-20 01:35:36 +0000

    Add 37.go

---
+++ src/37.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // Define variables
+    var x, y float64
+
+    // Set initial values
+    x = 2.0
+    y = 3.5
+
+    // Perform calculations
+    result := x + y
+
+    // Print the results
+    fmt.Println("The sum of", x, "and", y, "is:", result)
+}
